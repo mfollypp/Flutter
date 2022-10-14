7d34bd22d02bd4f1fcf6054ca08ce0fd53de46d2
Matheus Folly <matheusfolly@id.uff.br>
2022-10-14 00:16:11 -0300

    cleaning code

--- number_guess/lib/main.dart
+++ number_guess/lib/main.dart
@@ -19,17 +19,3 @@ void main() {
     },
   ));
 }
-
-
-// import 'package:number_guess/pages/home.dart';
-// import 'package:flutter/cupertino.dart';
-// import 'package:flutter/material.dart';
-
-// void main() {
-//     runApp(MaterialApp(
-//         title: "Guess the Number",
-//         debugShowCheckedModeBanner: false,
-//         theme: ThemeData(scaffoldBackgroundColor: Color.fromARGB(255, 88, 88, 88)),
-//         home: Home(),
-//     ));
-// }
